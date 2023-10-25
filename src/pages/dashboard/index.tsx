84eb82d7b3b8c4584c0055c103771b63e419b419
Mukhtar <m.abenov@pitech.kz>
2023-10-25 18:42:14 +0200

    fix: divided for components

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,18 @@
-const DashboardProps = {};
+import { Row, Col } from "antd";
+
+interface DashboardProps {}
 
 export const Dashboard: React.FC<DashboardProps> = () => {
-	return <div>Dashboard</div>;
+	return (
+		<>
+			<Row>
+				<Col span={24}>Line chart</Col>
+			</Row>
+			<Row>
+				<Col span={8}>Horizontal bar chart</Col>
+				<Col span={8}>Vertical bar chart</Col>
+				<Col span={8}>Pie chart</Col>
+			</Row>
+		</>
+	);
 };
